21f66f4ada696b7eff839ad43bc96fb3164ac1a1
auser509 <auser509@gmail.com>
2024-09-13 16:27:46 +0200

    x

--- invitacion-final/BodaJoseLuisyDebora/langs/js/es-ES.js
+++ invitacion-final/BodaJoseLuisyDebora/langs/js/es-ES.js
@@ -8,17 +8,14 @@ var lang_caracteresComentariosAsistencia = 'El mensaje no debe superar 100 carac
 var lang_asistenciaMsjExito_1 = 'Información enviada';
 var lang_asistenciaMsjExito_2 = 'Hemos enviado tu informacion de asistencia';
 
-
 var lang_cancionRequerida = 'Ingrese nombre de musica / autor';
 var lang_linkIncorrecto = 'Formato link incorrecto';
 var lang_enviandoSugerencia = 'Enviando sugerencia';
 var lang_sugerirCancion = 'Sugerir Canción';
 
-
 var lang_caracteresNombreSugerencia = 'El nombre no debe superar 20 caracteres';
 var lang_caracteresCancionSugerencia = 'El titulo/autor no debe superar 50 caracteres';
 var lang_caracteresLinkSugerencia = 'El link no debe superar 250 caracteres';
-
 
 var lang_sugerirCancionMsjExito_1 = 'Sugerencia enviada';
 var lang_sugerirCancionMsjExito_2 = 'Tu sugerencia de canción se ha agregado a la lista';
@@ -31,13 +28,9 @@ var lang_titleModalMapaCeremonia = 'Cómo llegar a la pre-boda'; // ok
 
 var lang_placeholderFiesta = "¿Vienes acompañado? Indica su nombre"; // modifico 11/03 - Indica algún dato importante. \n Ej: Soy vegano, vegetariano, etc.";
 var lang_placeholderCeremonia = "¿Vienes acompañado? Indica su nombre"; // modifico 11/03 - Ingrese algún dato importante";
-
+//
 var lang_textoCopiado = '¡IBAN Copiado!';
-
 var lang_codigoAccesoVipRequerido = 'Ingrese el código de acceso';
 var lang_btnCodigoVip = 'Acceder';
 var lang_accediendoCodigoVip = 'Accediendo';
-
-
 var lang_textoFinalCuentaRegresiva = '¡LLEGO EL <br> GRAN DÍA!';
-
